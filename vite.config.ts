c64e078fb7e8986dfdf5b7ae667c03899c10d08f
Tim Spears <timspears@imarc.com>
2023-10-05 15:23:18 -0400

    fix aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite'
+import path from 'path'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
@@ -6,14 +7,14 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@components': '/src/components',
-      '@context': '/src/context',
-      '@hooks': '/src/hooks',
-      '@icons': '/src/assets/icons',
-      '@images': '/src/assets/images',
-      '@styles': '/src/assets/styles',
-      '@types': '/src/types',
-      '@utils': '/src/utils',
+      '@components': path.resolve(__dirname,'src/components'),
+      '@context': path.resolve(__dirname,'src/context'),
+      '@hooks': path.resolve(__dirname,'src/hooks'),
+      '@icons': path.resolve(__dirname,'src/assets/icons'),
+      '@images': path.resolve(__dirname,'src/assets/images'),
+      '@styles': path.resolve(__dirname,'src/assets/styles'),
+      '@types': path.resolve(__dirname,'src/types'),
+      '@utils': path.resolve(__dirname,'src/utils'),
     }
   },
 })
